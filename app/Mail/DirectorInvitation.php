97996ea1e27d2bd7c2aa298de4ba2fadbbd71f2b
getachewabebe <gechachin@gmail.com>
2026-06-11 11:23:08 +0300

    Some Important Uodates

--- app/Mail/DirectorInvitation.php
+++ app/Mail/DirectorInvitation.php
@@ -2,26 +2,20 @@
 
 namespace App\Mail;
 
+use App\Models\Invitation;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 
 class DirectorInvitation extends Mailable
 {
-
     public $invitation;
 
-    /**
-     * Create a new message instance.
-     */
-    public function __construct(\App\Models\Invitation $invitation)
+    public function __construct(Invitation $invitation)
     {
         $this->invitation = $invitation;
     }
 
-    /**
-     * Get the message envelope.
-     */
     public function envelope(): Envelope
     {
         return new Envelope(
@@ -29,9 +23,6 @@ class DirectorInvitation extends Mailable
         );
     }
 
-    /**
-     * Get the message content definition.
-     */
     public function content(): Content
     {
         return new Content(
@@ -40,8 +31,6 @@ class DirectorInvitation extends Mailable
     }
 
     /**
-     * Get the attachments for the message.
-     *
      * @return array<int, \Illuminate\Mail\Mailables\Attachment>
      */
     public function attachments(): array
